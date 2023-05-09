6922c07bd719c36420a242d6068f73db79c22ca0
shanego416 <88070204+shanego416@users.noreply.github.com>
2023-05-09 08:48:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-document.getElementsByClassName("btn").onclick = paid;
+const buttons = document.getElementsByClassName("btn");
+const amountRemaining = document.querySelector("#amountRemaining");
+const amountSaved = document.querySelector("#amountSaved");
 
+let amountChange = false;
+
+// ASK COLLIN ABOUT the " => " OPERATOR
+Array.from(buttons).forEach((element) => {
+  element.addEventListener("click", paid);
+  element.addEventListener("click", subtract);
+  element.addEventListener("click", add);
+});
+
+// CHANGE THE LOOK OF THE BUTTON
 function paid() {
-  document.querySelector("body").style.backgroundColor = "red";
+  this.classList.toggle("active");
+}
+
+// SUBTRACT AMOUNT FROM AMOUNT REMAINING
+function subtract() {
+  let buttonNum = this.innerText.substring(1);
+  let num = parseInt(buttonNum);
+  let amountRemainNum = parseInt(amountRemaining.textContent);
+  let remainNum = amountRemainNum - num;
+  amountRemaining.textContent = remainNum;
+}
+
+// ADD AMOUNT TO AMOUNT SAVED
+function add() {
+  let buttonNum = this.innerText.substring(1);
+  let num = parseInt(buttonNum);
+  let amountSavedNum = parseInt(amountSaved.textContent);
+  let savedNum = amountSavedNum + num;
+  amountSaved.textContent = savedNum;
+
+  console.log(num);
 }
