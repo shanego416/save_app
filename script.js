ab196143abd2087c7e4a0ea6264a2796ba014989
shanego416 <88070204+shanego416@users.noreply.github.com>
2023-05-14 21:36:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const buttons = document.getElementsByClassName("btn");
+const buttons = document.querySelectorAll(".btn");
 const amountRemaining = document.querySelector("#amountRemaining");
 const amountSaved = document.querySelector("#amountSaved");
 
 let buttonClicked = {};
 let buttonNum = {};
+
+if (localStorage.getItem("Tester") !== null) {
+  amountSaved.textContent = localStorage.getItem("Tester");
+} else {
+  amountSaved.textContent = "0.00";
+}
 
 // ASK COLLIN ABOUT the " => " OPERATOR
 Array.from(buttons).forEach((element) => {
@@ -21,19 +27,27 @@ function updateAmountRemaining() {
   let newButtonNum = this.innerText.substring(1);
   let num = parseInt(newButtonNum);
   let amountRemainNum = parseInt(amountRemaining.textContent);
+  let amountSavedNum = parseInt(amountSaved.textContent);
 
   if (buttonClicked[this.id] === true) {
     let remainNum = amountRemainNum + num;
     amountRemaining.textContent = remainNum;
+    let savedNum = amountSavedNum - num;
+    amountSaved.textContent = savedNum;
     buttonClicked[this.id] = false;
     console.log(1);
-    console.log("Changed to false");
+    localStorage.setItem("Tester", amountSaved.textContent);
   } else {
     let remainNum = amountRemainNum - num;
     amountRemaining.textContent = remainNum;
+    let savedNum = amountSavedNum + num;
+    amountSaved.textContent = savedNum;
     buttonClicked[this.id] = true;
     console.log(2);
-    console.log("Changed to true");
+    localStorage.setItem("Tester", amountSaved.textContent);
   }
   buttonNum[this.id] = num;
 }
+
+// UPDATE AMOUNT SAVED
+//localStorage.clear();
